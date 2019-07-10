90d7b5aaac179844f72f00fe20863a3534e1dcdf
Kelechi Ogbonna <kellogbonna@gmail.com>
2019-07-10 13:18:56 +0100

    Add logger middleware

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // code away!
+const server = require('./server');
+
+server.listen(4100, () => console.log('server lisetning on port 4100'));
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-const express = 'express';
+const express = require('express');
 
 const server = express();
 
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`)
+  res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
 //custom middleware
 
 function logger(req, res, next) {
+  console.log(
+    `[${newDate().toISOString()}] ${req.method} to ${req.url} from ${req.get(
+      'Origin'
+    )}`
+  );
+  next();
+}
 
-};
+server.use(logger);
 
 module.exports = server;
